a61388a052b8a691b34841811d0e3fc2f5942beb
volmaks.Dev <155001237+volmaksDev@users.noreply.github.com>
2025-12-28 22:43:30 +0200

    Update UI.lua

--- Additional/UI.lua
+++ Additional/UI.lua
@@ -373,7 +373,7 @@ local Compkiller = {
 };
 
 Compkiller.Colors = {
-	Highlight = Color3.fromRGB(105,105,105),
+	Highlight = Color3.fromRGB(0,0,0),
 	Toggle = Color3.fromRGB(14, 203, 213),
 	Risky = Color3.fromRGB(251, 255, 39),
 	BGDBColor = Color3.fromRGB(22, 24, 29),
